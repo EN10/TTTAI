b15bde23d8867d2b3719a4a27dc08e9499d078b0
Enio F <sfm.neo@gmail.com>
2014-07-06 16:17:35 +0000

    ./ static

--- web.js
+++ web.js
@@ -21,10 +21,7 @@ app.get("/", function(req, res) {
 });
 
  /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile(__dirname + req.params[0]); 
- })
+ app.use(express.static(__dirname + '/static'));
 
  var port = process.env.PORT || 80;
  app.listen(port, function() {
