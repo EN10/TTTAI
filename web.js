c1d021539039506f6a94fd3ece0e3540a73f6f1a
Enio F <sfm.neo@gmail.com>
2014-07-06 16:05:29 +0000

    serve static files

--- web.js
+++ web.js
@@ -20,6 +20,12 @@ app.get("/", function(req, res) {
   }
 });
 
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile(__dirname + req.params[0]); 
+ })
+
  var port = process.env.PORT || 80;
  app.listen(port, function() {
    console.log("Listening on " + port);
