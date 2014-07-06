8d180de7b8c2bf86c5bfc0e72ea85ffd15b5b5de
Enio F <sfm.neo@gmail.com>
2014-07-06 17:36:57 +0000

    reorder

--- web.js
+++ web.js
@@ -1,29 +1,24 @@
 var express = require("express");
 var app = express();
+
 app.get("/", function(req, res) {
-    
-    var fs = require('fs');
-  { var arr = fs.readFileSync('games.txt').toString().split(",");
-  
-    if (req.query.q == 'v' )                // Output Games to HTML
-    {   var html = '';
-        for (var i=0; i<arr.length; i++)
-        {   html = html + arr[i] + " ";   }
-        res.send(html);
-    }   
-        
-    else if (req.query.g !== undefined)     // Insert Game
-    {   fs.appendFileSync('games.txt', req.query.g + ",");  }
-        
-    else {  res.sendfile('index.html'); }   // Display Board
-        
-  }
+  var fs = require('fs');
+  if (req.query.q == 'v' )                // Output Games to HTML
+  {   var arr = fs.readFileSync('games.txt').toString().split(",");
+   var html = '';
+   for (var i=0; i<arr.length; i++)
+   {   html = html + arr[i] + " ";   }
+   res.send(html);
+  }   
+  else if (req.query.g !== undefined)     // Insert Game
+  {   fs.appendFileSync('games.txt', req.query.g + ",");  }
+  else {  res.sendfile('index.html'); }   // Display Board 
 });
 
- /* serves all the static files */
- app.use(express.static(__dirname));
+/* serves all the static files */
+app.use(express.static(__dirname));
 
- var port = process.env.PORT || 80;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+var port = process.env.PORT || 80;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
