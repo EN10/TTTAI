18f0a3ae86bd124ce74026840b2ea3c9edb95eef
Enio <sfmneo@gmail.com>
2016-02-09 12:39:30 +0000

    no duplicates in file

--- web.js
+++ web.js
@@ -11,12 +11,15 @@ app.get("/", function(req, res) {
     res.send(html);
   }   
     else if (req.query.g !== undefined)           // Insert Game
-  { fs.appendFileSync('games.txt',req.query.g + "\n");
-    var games = fs.readFileSync('games.txt').toString().split("\n");
+  { var games = fs.readFileSync('games.txt').toString().split("\n");
+    if (games.indexOf(req.query.g) == -1){
+    fs.appendFileSync('games.txt',req.query.g + "\n");
+    games = fs.readFileSync('games.txt').toString().split("\n");
     games.sort();
     fs.writeFileSync('games.txt',games[1] + "\n");
     for (var i=2; i<games.length; i++)
     { fs.appendFileSync('games.txt',games[i] + "\n"); }
+    }
   }
   else {  res.sendfile('index.html'); }   // Display Board 
 });
